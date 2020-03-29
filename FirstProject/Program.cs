b795ec3b6b81203c4af0b1c0806076318773a9ab
Bob Cohen <bobcohen1969@yahoo.com>
2020-03-29 15:40:26 -0500

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Bob"; //const means the variable can't chnage.  need to assign value to it when creating it
+            Console.WriteLine(name);
+
+            float number = 2.5f;  //need to include the 'f' at end of value
+            decimal decNumber = 2.5m; //need to include the 'm' at end of value 
+        }
+    }
+}
